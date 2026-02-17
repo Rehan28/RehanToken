ec384f5fa875ac7a7cd0f962bbdc8b7875ad03ee
Rakibul Islam Rehan <rakibulislamrehan@gmail.com>
2026-02-18 03:35:39 +0600

    FInal

--- contracts/RehanToken.sol
+++ contracts/RehanToken.sol
@@ -5,21 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RehanToken is ERC20, Ownable {
-
-    constructor(uint256 initialSupply)
-        ERC20("RehanToken", "RHT")
-        Ownable(msg.sender)
-    {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+    constructor(uint256 initialSupply) ERC20("RehanToken", "RHT") {
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
     }
 
-    // Mint new tokens to a specified address (only owner)
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
+        _mint(to, amount * (10 ** uint256(decimals())));
     }
 
-    // Burn tokens from the caller's balance
     function burn(uint256 amount) public {
-        _burn(msg.sender, amount * 10 ** decimals());
+        _burn(msg.sender, amount * (10 ** uint256(decimals())));
     }
 }
